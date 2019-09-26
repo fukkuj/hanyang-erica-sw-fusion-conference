6a552a5455477d5e7c88572e182228512f6658b1
mthekid <gasman87@daum.net>
2019-09-26 10:09:49 +0900

    change Environment happenning
    mac 잡업을 올리던 중 문제가 생겼습니다

--- ai/AI.py
+++ ai/AI.py
@@ -47,10 +47,12 @@ class AI():
             x = (x - 128) / 128
             x = torch.FloatTensor(x).cuda()
             
-            if self.detect(x):
-                result = int(self.classify(x))
-            else:
-                result = -1
+            result = int(self.classify(x))
+
+            #if self.detect(x):
+            #    result = int(self.classify(x))
+            #else:
+            #    result = -1
                 
         return result
             
@@ -60,9 +62,9 @@ class AI():
         logps = self.detector(x)
         ps = torch.exp(logps)
 
-        # print(ps[:, 1])
+        print(ps[:, 1])
         
-        ratio = ps[:, 1] >= 0.9
+        ratio = ps[:, 1] < 0.1
         ratio = torch.mean(ratio.type(torch.FloatTensor))
         
         # if 6 or more pictures have trash,
